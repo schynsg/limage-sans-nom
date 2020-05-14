426db4e19ef467846936be4b6e7042c4cae176fd
Goran <goran.schyns@student.hepl.be>
2020-05-14 13:00:08 +0200

    Create the image animation

---
+++ src/js/index-images-animation.js
@@ -0,0 +1,33 @@
+const animationImage = document.getElementById('animated-image');
+
+let imageCount = 3;
+let imageNumber = 1;
+let imageSrc = './src/img/intro-img' + imageNumber + '.jpg';
+
+
+function getImageSrc(){
+    if (imageNumber < imageCount) {
+        imageNumber++;
+    } else if (imageNumber >= imageCount){
+        imageNumber = 1;
+    }
+    imageSrc = './src/img/intro-img' + imageNumber + '.jpg';
+};
+
+function changeBtnColor() {
+    const imageBtn = document.getElementsByClassName('intro-images__item');
+    for (let i = 0; i < imageCount; i++) {
+        imageBtn[i].classList.remove("active");
+    };
+    imageBtn[imageNumber-1].classList.add('active');
+}
+
+function changeImage(){
+    getImageSrc();
+    changeBtnColor();
+    animationImage.src = imageSrc;
+};
+
+
+
+setInterval(changeImage, 4000);
